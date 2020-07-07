172238e6bbd69a0c11157cba0680f6a3af56a3a8
Ilya Stepanov <iliastepanov1996@gmail.com>
2020-07-07 17:17:08 +0200

    Test stéréotypies prêt

--- components/Eleveur/Tests/Stereotypies.js
+++ components/Eleveur/Tests/Stereotypies.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { View, Text, StyleSheet, TouchableWithoutFeedback, Dimensions, Alert } from 'react-native';
+import { View, Text, StyleSheet, TouchableWithoutFeedback, Dimensions, Alert, Keyboard } from 'react-native';
 import { useDispatch } from 'react-redux';
 import Counter from '../../UI/Counter';
 import ProgressBar from 'react-native-progress/Bar';
@@ -84,77 +84,89 @@ const Stereotypies = props => {
 
     return (
         <View>
-            <View style={styles.counterContainer}>
-                <View style={{ flexDirection: 'row' }}>
-                    <Text style={styles.counterText}>   {globalCount} / {nbTruies} </Text>
-                    <TouchableWithoutFeedback onPress={() => {
-                        setModalEchantillonVisible(true);
-                    }}>
-                        <EvilIcons name="question" size={30} color="black" />
-                    </TouchableWithoutFeedback>
-                </View>
-                <ProgressBar progress={globalCount / nbTruies} width={200} />
-            </View>
-            <View style={{ height: Dimensions.get('window').height / 1.60 }}>
+            <TouchableWithoutFeedback onPress={() => { Keyboard.dismiss() }}>
                 <View>
-                    <View>
-                        <Text style={styles.text}>
-                            <Text style={{ fontSize: 25 }}>• {" "}</Text>
+                    <View style={styles.container}>
+                        <View style={styles.headerContainer}>
+                            <View style={{ flexDirection: 'row' }}>
+                                <Text style={styles.counterText}>   {globalCount} / {nbTruies} </Text>
+                                <TouchableWithoutFeedback onPress={() => {
+                                    setModalEchantillonVisible(true);
+                                }}>
+                                    <EvilIcons name="question" size={30} color="black" />
+                                </TouchableWithoutFeedback>
+                            </View>
+                            <ProgressBar progress={globalCount / nbTruies} width={200} />
+                        </View>
+
+                        <View style={{ flex: 1, justifyContent: 'center' }}>
+                            <View>
+                                <View>
+                                    <Text style={styles.text}>
+                                        <Text style={{ fontSize: 25 }}>• {" "}</Text>
                                 Pas de comportement de stéréotypie observé
                             </Text>
-                    </View>
-                    <View style={styles.content}>
-                        <Counter onChange={changeHandler} max={nbTruies} />
-                    </View>
-                </View>
+                                </View>
+                                <View style={styles.content}>
+                                    <Counter onChange={changeHandler} max={nbTruies} />
+                                </View>
+                            </View>
 
-                <View style={{ marginVertical: 25 }}>
-                    <View>
-                        <Text style={styles.text}>
-                            <Text style={{ fontSize: 25 }}>• {" "}</Text>
+                            <View style={{ marginVertical: 25 }}>
+                                <View>
+                                    <Text style={styles.text}>
+                                        <Text style={{ fontSize: 25 }}>• {" "}</Text>
                                 Mise en évidence de comportement de stéréotypie observé
                             </Text>
+                                </View>
+                                <View style={styles.content}>
+                                    <Counter onChange={changeHandler2} max={nbTruies} />
+                                </View>
+                            </View>
+                        </View>
                     </View>
-                    <View style={styles.content}>
-                        <Counter onChange={changeHandler2} max={nbTruies} />
-                    </View>
-                </View>
-            </View>
 
 
-            {/*Modal infos sur l'évaluation*/}
-            <ModalPopupInfo
-                visible={modalInfoVisible}
-                onClose={modalInfoCloser}
-                text={<Text>Le comportement de stéréotypie est défini comme la séquence d'actes moteurs invariants qui n'apporte pas de gain évident au but pour l'animal.{"\n"}
+                    {/*Modal infos sur l'évaluation*/}
+                    <ModalPopupInfo
+                        visible={modalInfoVisible}
+                        onClose={modalInfoCloser}
+                        text={<Text>Le comportement de stéréotypie est défini comme la séquence d'actes moteurs invariants qui n'apporte pas de gain évident au but pour l'animal.{"\n"}
                             les stéréotypies évaluées sont les faux mâchonnements, le roulement de langue, le grincement de dents, les morsures d'abreuvoirs, le lèchement de sol.{"\n"}
                             Chaque truie doit être observée 15 secondes au minimum.
                         </Text>}
-                buttonText='Fermer'
-            />
-            {/*Modal infos sur la composition de l'échantillon*/}
-            <ModalPopupInfo
-                visible={modalEchantillonVisible}
-                onClose={modalEchantillonCloser}
-                text="40 truies (dont 1/3 en début de gestation, 1/3 en milieu de gestation et 1/3 en fin de gestation)."
-                buttonText='Fermer'
-            />
-            {/*Modal pour la confirmation de la validation*/}
-            <ModalPopupInfo
-                visible={modalConfirmation}
-                onClose={modalConfirmationCloser}
-                text='Valider définitivement les données saisies ?'
-                buttonText='Annuler'
-                confirmation
-                onValidation={validationHandler}
-            />
+                        buttonText='Fermer'
+                    />
+                    {/*Modal infos sur la composition de l'échantillon*/}
+                    <ModalPopupInfo
+                        visible={modalEchantillonVisible}
+                        onClose={modalEchantillonCloser}
+                        text="40 truies (dont 1/3 en début de gestation, 1/3 en milieu de gestation et 1/3 en fin de gestation)."
+                        buttonText='Fermer'
+                    />
+                    {/*Modal pour la confirmation de la validation*/}
+                    <ModalPopupInfo
+                        visible={modalConfirmation}
+                        onClose={modalConfirmationCloser}
+                        text='Valider définitivement les données saisies ?'
+                        buttonText='Annuler'
+                        confirmation
+                        onValidation={validationHandler}
+                    />
+                </View>
+            </TouchableWithoutFeedback>
         </View>
     );
 };
 
 
 const styles = StyleSheet.create({
-    counterContainer: {
+    container: {
+        marginTop: 20,
+        height: Dimensions.get('window').height / 1.60,
+        alignItems: 'center'
+    },
+    headerContainer: {
         alignItems: 'center',
         marginBottom: 35,
     },

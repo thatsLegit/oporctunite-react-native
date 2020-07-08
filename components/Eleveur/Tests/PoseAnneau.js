2073d3a117a2a9d2f830ec075dfd1a1d556e6f40
Ilya Stepanov <iliastepanov1996@gmail.com>
2020-07-08 10:54:21 +0200

    Eval Pose d'anneaux nasaux et coupe de queue terminée

---
+++ components/Eleveur/Tests/PoseAnneau.js
@@ -0,0 +1,163 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { View, Text, StyleSheet, TouchableWithoutFeedback, Dimensions, Alert, Keyboard } from 'react-native';
+import { useDispatch } from 'react-redux';
+import Counter from '../../UI/Counter';
+import { FontAwesome } from '@expo/vector-icons';
+import ModalPopupInfo from '../../../components/Eleveur/Evaluations/ModalPopupInfo';
+import { EvilIcons } from '@expo/vector-icons';
+import * as testActions from '../../../store/actions/test';
+
+
+const PoseAnneau = props => {
+
+    const { modalInfo, confirmation, navigation, nomEvaluation, Vtype } = props;
+    const [modalEchantillonVisible, setModalEchantillonVisible] = useState(false);
+    const [modalInfoVisible, setModalInfoVisible] = useState(modalInfo);
+    const [modalConfirmation, setModalConfirmation] = useState(confirmation);
+    const [count, setCount] = useState(0);
+    const [count2, setCount2] = useState(0);
+    const [count3, setCount3] = useState(0);
+    const [globalCount, setGlobalCount] = useState(0);
+
+    const dispatch = useDispatch();
+
+    const note = Math.round((((count / globalCount) * 10 + (count2 / globalCount) * 5 + Number.EPSILON) * 10)) / 10;
+
+    const validationHandler = async () => {
+        await dispatch(testActions.ajouterTest(note, nomEvaluation));
+
+        if (Vtype == 'valider') {
+            modalConfirmationCloser();
+            navigation.navigate('TestRecap');
+        } else { //Suivant case
+            props.onNextValidation();
+        }
+    };
+
+    const changeHandler = (count, sign, value) => {
+        setCount(count);
+        if (sign == 'plus') {
+            setGlobalCount(globalCount + value);
+        } else {
+            setGlobalCount(globalCount - value);
+        }
+    };
+    const changeHandler2 = (count2, sign, value) => {
+        setCount2(count2);
+        if (sign == 'plus') {
+            setGlobalCount(globalCount + value);
+        } else {
+            setGlobalCount(globalCount - value);
+        }
+    };
+    const changeHandler3 = (count3, sign, value) => {
+        setCount3(count3);
+        if (sign == 'plus') {
+            setGlobalCount(globalCount + value);
+        } else {
+            setGlobalCount(globalCount - value);
+        }
+    };
+
+    const modalInfoCloser = () => {
+        setModalInfoVisible(false);
+        props.onCloseInfo();
+    };
+    const modalConfirmationCloser = useCallback(() => {
+        setModalConfirmation(false); //local component
+        props.onCloseConfirmation(); //parent component
+    });
+
+    useEffect(() => {
+        setModalInfoVisible(modalInfo);
+        if (confirmation && globalCount > 0) {
+            setModalConfirmation(confirmation);
+        }
+        if (confirmation && globalCount == 0) {
+            modalConfirmationCloser();
+            Alert.alert('Erreur', `Il faut réaliser l'évaluation sur au moins 1 animal.`, [{ text: 'Compris', style: 'destructive' }]);
+        }
+    }, [modalInfo, confirmation, globalCount]);
+
+
+    return (
+        <View>
+            <TouchableWithoutFeedback onPress={() => { Keyboard.dismiss() }}>
+                <View style={styles.container}>
+                    <View>
+                        <View>
+                            <Text style={styles.text}>
+                                <Text style={{ fontSize: 25 }}>• {" "}</Text>
+                                Pas de mutilations réalisées
+                            </Text>
+                        </View>
+                        <View style={styles.counter}>
+                            <Counter onChange={changeHandler} max={null} />
+                        </View>
+                    </View>
+
+                    <View style={{ marginVertical: 25 }}>
+                        <View>
+                            <Text style={styles.text}>
+                                <Text style={{ fontSize: 25 }}>• {" "}</Text>
+                                Les mutilations sont réalisées avec l'utilisation d'anesthésiques
+                            </Text>
+                        </View>
+                        <View style={styles.counter}>
+                            <Counter onChange={changeHandler2} max={null} />
+                        </View>
+                    </View>
+
+                    <View style={{ marginVertical: 25 }}>
+                        <View>
+                            <Text style={styles.text}>
+                                <Text style={{ fontSize: 25 }}>• {" "}</Text>
+                                Les mutilations sont réalisées sans l'utilisation d'anesthésiques ou anelgésiques
+                            </Text>
+                        </View>
+                        <View style={styles.counter}>
+                            <Counter onChange={changeHandler3} max={null} />
+                        </View>
+                    </View>
+                </View>
+            </TouchableWithoutFeedback>
+
+            {/*Modal infos sur l'évaluation*/}
+            <ModalPopupInfo
+                visible={modalInfoVisible}
+                onClose={modalInfoCloser}
+                text="L'évaluateur enregistre la façon dont les mutilations sont entreprises et si des anesthésiques et analgésiques sont utilisées durant le processus."
+                buttonText='Fermer'
+            />
+            {/*Modal pour la confirmation de la validation*/}
+            <ModalPopupInfo
+                visible={modalConfirmation}
+                onClose={modalConfirmationCloser}
+                text='Valider définitivement les données saisies ?'
+                buttonText='Annuler'
+                confirmation
+                onValidation={validationHandler}
+            />
+        </View>
+    );
+};
+
+
+const styles = StyleSheet.create({
+    container: {
+        height: Dimensions.get('window').height / 1.60,
+        justifyContent: 'center'
+    },
+    counter: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        marginTop: 20
+    },
+    text: {
+        fontFamily: 'open-sans',
+        fontSize: 17
+    }
+});
+
+
+export default PoseAnneau;

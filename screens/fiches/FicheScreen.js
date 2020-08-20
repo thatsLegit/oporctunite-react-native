22ed8ddea3b6d191217c64a1acef4f702e699894
Ilya Stepanov <iliastepanov1996@gmail.com>
2020-08-20 14:52:39 +0200

    ultime essai

--- screens/fiches/FicheScreen.js
+++ screens/fiches/FicheScreen.js
@@ -197,6 +197,7 @@ const FicheScreen = props => {
                     <PDFReader
                         noLoader={false}
                         withPinchZoom={true}
+                        withScroll={true}
                         source={{
                             uri: content
                         }}
